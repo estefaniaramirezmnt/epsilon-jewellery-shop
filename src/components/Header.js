5de99b96a232528448351c59265630c3f73d6ca4
Estefanía <estefaniaramirezmnt@gmail.com>
2024-05-10 11:29:09 +0200

    Added blur effect to logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,22 @@
-import React from 'react'
-// import logo from './public/epsilon-logo-black.png'
-import logo from '../img/epsilon-logo-black.png'
+import React, { useEffect, useState } from 'react';
+import logo from '../img/epsilon-logo-black.png';
 
 export default function Header() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoaded(true);
+    }, 1000);
+  }, []);
+
   return (
     <div className="header">
-        <img src={logo} alt="Epsilon Logo" className='header-logo'/>
+        <img 
+          src={logo} 
+          alt="Epsilon Logo" 
+          className={`header-logo ${isLoaded ? 'loaded' : ''}`} 
+        />
     </div>
-  )
+  );
 }
-
