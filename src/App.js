32346dd99fec0b58f963a4dbe80ca1d12d5328c2
Estefanía <estefaniaramirezmnt@gmail.com>
2024-05-13 19:48:17 +0200

    Separated navbar in new component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 import "./css/style.css";
 
 function App() {
   return (
     <div className="bg-[#181818] w-screen h-screen overflow-hidden">
       <Header />
-      <div className="w-screen py-6 px-5 lg:px-64 bg-[#252525] flex justify-center text-neutral-300">
-        <ul className="flex justify-between w-1/2 text-center">
-          <li>About</li>
-          <li>Contact</li>
-          <li>Earrings</li>
-          <li>Help</li>
-        </ul>
-      </div>
+      <Navbar />      
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="w-screen py-6 px-5 lg:px-64 bg-[#252525] flex justify-center text-neutral-300">
+      <ul className="flex justify-between w-1/2 text-center">
+        <li>Home</li>
+        <li>Contact</li>
+        <li>Earrings</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
