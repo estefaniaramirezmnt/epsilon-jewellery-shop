567b492d0457abf94bdeb3f2af9f45450d47b482
Estefanía <estefaniaramirezmnt@gmail.com>
2024-05-13 19:20:06 +0200

    Installed tailwind css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import "./css/style.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-[#181818] w-screen h-screen overflow-hidden">
       <Header />
+      <div className="w-screen py-6 px-5 bg-[#252525] flex justify-between text-neutral-300">
+        
+      </div>
     </div>
   );
 }
