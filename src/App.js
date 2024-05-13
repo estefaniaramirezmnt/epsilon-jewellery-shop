a2fd26c7440062b3b933888e253c3ae3c797434d
Estefanía <estefaniaramirezmnt@gmail.com>
2024-05-13 19:34:21 +0200

    Added name of link

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ import "./css/style.css";
 function App() {
   return (
     <div className="bg-[#181818] w-screen h-screen overflow-hidden">
-    <Header />
-    <div className="w-screen py-6 px-5 lg:px-64 bg-[#252525] flex justify-center text-neutral-300">
-      <ul className="flex justify-between w-1/2 text-center">
-        <li>About</li>
-        <li>Contact</li>
-        <li>Service</li>
-        <li>Help</li>
-      </ul>
+      <Header />
+      <div className="w-screen py-6 px-5 lg:px-64 bg-[#252525] flex justify-center text-neutral-300">
+        <ul className="flex justify-between w-1/2 text-center">
+          <li>About</li>
+          <li>Contact</li>
+          <li>Earrings</li>
+          <li>Help</li>
+        </ul>
+      </div>
     </div>
-  </div>
-);
+  );
 }
 
 export default App;
