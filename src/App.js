951d34e2189e8ca5446d4f8e0021c72924e8c90a
Estefanía <estefaniaramirezmnt@gmail.com>
2024-05-20 16:27:40 +0200

    Added router dom and crete pages for links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import {Routes, Route, Link} from 'react-router-dom';
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Earrings from "./components/pages/Earrings";
+import Contact from "./components/pages/Contact";
 
 function App() {
   return (
     <div className="bg-[#181818] w-screen h-screen overflow-hidden">
       <Header />
+      <nav className="navbar-menu">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/earrings">Earrings</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+        
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/earrings" element={<Earrings />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/pages/Earrings.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Earrings() {
+  return (
+    <div>Earrings</div>
+  )
+}
+
+export default Earrings
